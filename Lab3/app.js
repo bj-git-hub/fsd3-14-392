14d9249edf6ac06d8bfebb05d338ee47a08606fb
bj-git-hub <joshi.bhavesh.in@gmail.com>
2026-09-08 10:09:18 +0530

    add helper functions

--- Lab3/app.js
+++ Lab3/app.js
@@ -1,11 +1,33 @@
 import http from "http";
+import { getAllTeams } from "./teams.js";
+import { resolve } from "dns";
+import {parse as parseUrl} from "url";
 
-const server = http.createServer();
-server.on('request', (req, res) => {
-  //on Event Emitter
-  res.end("<h1> SIH Internal </h1>")
+const PORT = 5000;
+
+const sendJson = (res, statusCode, data) => {
+  res.writeHead(statusCode, { "content-type": "application/json" });
+  res.end(data === "undefined" ? "" : JSON.stringify(data));
+};
+const parseJSONBody = (req) => {
+  new Promise((resolve, reject) => {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      try {
+        resolve(bosy ? JSON.parse(body) : {});
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+};
+const server = http.createServer((req, res) => {
+  
 });
 
-server.listen(5000, () => {
-  console.log("SIH Server is Running");
+server.listen(PORT, () => {
+  console.log("SIH Sever is running at ", PORT);
 });
