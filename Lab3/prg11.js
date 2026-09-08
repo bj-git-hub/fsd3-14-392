b8c15c330651d00c3c08ed2d4da37c44fec96002
bj-git-hub <joshi.bhavesh.in@gmail.com>
2026-09-08 09:02:53 +0530

    add routes

--- Lab3/prg11.js
+++ Lab3/prg11.js
@@ -1,50 +1,53 @@
-// Routes
-import { log } from "console";
 import http from "http";
 
 const server = http.createServer((req, res) => {
+  if (req.url === "/" && req.method === "GET") res.end("home page");
+  else if (req.url === "/product" && req.method === "GET") {
+    const products = [
+      {
+        id: 1,
+        name: "mobile",
+        price: 2000,
+      },
+      {
+        id: 2,
+        name: "duster",
+        price: 10,
+      },
+    ];
+    res.end(JSON.stringify(products));
+  } else if (req.url === "/product" && req.method === "POST") {
+    // retrieve data from client
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    // console.log("body", body);
+    // res.end();
 
-    if(req.url === '/' && req.method === 'GET'){
-        res.end("This is the home page.");
-        
-    }
-    else if(req.url === '/product' && req.method === 'GET'){
-        const products = [
-          {
-            id: 1,
-            name: "mobile",
-            price: 2000,
-          },
-          {
-            id: 2,
-            name: "duster",
-            price: 200,
-          },
-        ];
-        res.end(JSON.stringify(products));
-    }
-    else if(req.url === '/product' && req.method === 'POST'){
-        // retrieve data from client
-        let body ="";
-        req.on("data", (chunk) => {
-            body += chunk;
-        });
+    req.on("end", () => {
+      const product = JSON.parse(body);
 
-        req.on("end,()")
-        res.end("Add Product");
-    }
-    else if(req.url === '/product' && req.method === 'PUT'){
-        res.end("Update Product");
-    }
-    else if(req.url === '/product' && req.method === 'DELETE'){
-        res.end("Remove Product");
-    }
-    else{
-        res.statusCode = 404;
-        res.end("Not Found");
-    }
-   
-
+      //add data to database
+      res.writeHead(201, {
+        "content-type": "application/json",
+      });
+      //send back the status
+      res.end(
+        JSON.stringify({
+          msg: "product added",
+          product,
+        }),
+      );
+    });
+  } else if (req.url === "/product" && req.method === "PUT")
+    res.end("update product");
+  else if (req.url === "/product" && req.method === "DELETE")
+    res.end("remove product");
+  else {
+    res.statusCode = 404;
+    res.end("not found");
+  }
 });
 
-server.listen(3000, () => console.log("Program 11 is running"));
+server.listen(3000, () => console.log("prg11 is running"));
