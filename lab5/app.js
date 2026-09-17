fb08e1e574bb0d6606624d7a55951f618b06a863
bj-git-hub <joshi.bhavesh.in@gmail.com>
2026-09-17 13:59:09 +0530

    basic routing with express server

---
+++ lab5/app.js
@@ -0,0 +1,45 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1> Hello Express</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("We are FSD developers");
+});
+
+app.post("/login", (req, res) =>{
+  res.send({
+    msg: 'user login'
+  })
+})
+
+app.put("/user/update/1", (req, res) => {
+  res.send({
+    msg: "user update"
+  });
+});
+
+app.delete("/users/1", (req, res) => {
+  res.send({
+    msg: "remove user 1",
+  });
+});
+
+app.post("/login", (req, res) => {
+  res.send({
+    msg: "user login",
+  });
+});
+
+
+
+
+app.use((req, res) =>{
+  res.status(404).send("Not Found")
+})
+
+app.listen(2000, () => console.log("Server is running"));
+
